d781e1fee2f2b4024c03c004ae0839e670fdec4f
Lotoz <zamira.grippi@gmail.com>
2025-02-13 12:46:44 +0100

    13/02

---
+++ Tema 4/PracticaDOM/assets/codigo.js
@@ -0,0 +1,11 @@
+//Primero se ha realizado las cajas sin fotografia
+//Capa 1
+const evento1 = document.querySelector('.cambiarColor');
+
+//Funcion
+//En esta capa cuando pasa el raton por encima la letra cambia de color
+evento1.addEventListener("mouseenter", () => {evento1.style.color = "red";});
+evento1.addEventListener("mouseleave", () => {evento1.style.color = "black";});
+
+//Capa3
+
