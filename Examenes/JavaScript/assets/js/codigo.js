3ad7c35c740d16a60015d9f826cb7578bc7eed4a
Lotoz <zamira.grippi@gmail.com>
2025-03-19 14:39:31 +0100

    a

--- Examenes/JavaScript/assets/js/codigo.js
+++ Examenes/JavaScript/assets/js/codigo.js
@@ -1,69 +1,59 @@
-
 /**
  * Para buscar palabra. Examen
  */
 
 //Mensaje de error
-const textError = document.querySelector('#text');
+const textError = document.querySelector('#text')
 
 /*
-* Funcion que muestra un mensaje de error
-*/
-function mostrarError(mensaje) {
-    textError.textContent = mensaje;
+ * Funcion que muestra un mensaje de error
+ */
+function mostrarError (mensaje) {
+  textError.textContent = mensaje
 }
 
 /*
-* Funcion que resetea los resultados
-*/
-function resetearResultados() {
-    textError.textContent = '';
+ * Funcion que resetea los resultados
+ */
+function resetearResultados () {
+  textError.textContent = ''
 }
 
 //Boton para buscar
-const button = document.querySelector('#boton');
+const button = document.querySelector('#boton')
 //Input del boton
-const buscarInput = document.querySelector('#inputBuscador');
+const buscarInput = document.querySelector('#inputBuscador')
 
 //Palabra buscada
-const palabraSalida = document.querySelector('#palabraBuscada');
+const palabraSalida = document.querySelector('#palabraBuscada')
 
-
-const originBuscado = document.getElementById('#originBuscado');
-
+const originBuscado = document.querySelector('#originBuscado')
 
 //Evento de búsqueda
-button.addEventListener("click", buscarPalabra);
-
+button.addEventListener('click', buscarPalabra)
 
 /**
  * Funcion para buscar una pelicula
  */
-async function buscarPalabra() {
-    resetearResultados()
-   
-    const buscado = buscarInput.value;
-    const url = (`https://rae-api.com/api/words/` + buscado);
-        try {
-       
-        const respuesta = await fetch(url);
-        const data = await respuesta.json();
+async function buscarPalabra () {
+  resetearResultados()
+  const url = `https://rae-api.com/api/words/${buscarInput.value}`
+  try {
+    palabraSalida.innerHTML = ''
+    originBuscado.innerHTML = ''
+    const respuesta = await fetch(url)
+    const data = await respuesta.json()
 
-        if (!data.ok) {
-            mostrarError('No se ha encontrado la palabra');
-        }
+    palabraSalida.style.color = 'green'
+    palabraSalida.innerHTML =
+      data.data.word + ` (${data.data.meanings[0].origin.raw})`
 
-        palabraSalida.innerText =  data.data.word + ` (${data.data.meanings[0].origin.raw})`;
-        
-        let significado;
-        data.data.meanings[0].senses.forEach(element => {
-        
-        significado += `<li>${element.raw}</li>`;
-        
-        });
-        originBuscado.innerHTML = significado;
-    
-        } catch (error) {
-            mostrarError('No se ha encontrado una palabra');
-        }
+    data.data.meanings[0].senses.forEach(element => {
+      originBuscado.innerHTML += `<li>${element.raw}</li>`
+    })
+  } catch (error) {
+    palabraSalida.style.color = 'red'
+    palabraSalida.innerHTML = 'No se ha encontrado una palabra'
+    originBuscado.innerHTML = ''
+  }
 }
